6737a61c69c3098507872db92343ad72c6938a5c
Alessio Parma <alessio.parma@gmail.com>
2015-01-24 15:13:44 +0100

    web api caching!

--- REST Service (WebAPI)/Controllers/ValuesController.cs
+++ REST Service (WebAPI)/Controllers/ValuesController.cs
@@ -1,39 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
+using WebApi.OutputCache.V2;
 
 namespace RestService.Mvc.Controllers
 {
-   public class ValuesController : ApiController
-   {
-      // GET api/values
-      public IEnumerable<string> Get()
-      {
-         return new string[] { "value1", "value2" };
-      }
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [CacheOutput(ServerTimeSpan = 60)]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
 
-      // GET api/values/5
-      public string Get(int id)
-      {
-         return "value";
-      }
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
 
-      // POST api/values
-      public void Post([FromBody]string value)
-      {
-      }
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
 
-      // PUT api/values/5
-      public void Put(int id, [FromBody]string value)
-      {
-      }
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
 
-      // DELETE api/values/5
-      public void Delete(int id)
-      {
-      }
-   }
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
 }
