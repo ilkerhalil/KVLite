dfecc5c3e2adce2307025ab6bff0ece56cb97b0a
Alessio Parma <alessio.parma@gmail.com>
2016-01-04 11:41:21 +0100

    config pkg sqlite

--- KVLite.WebApi/OutputCacheProvider.cs
+++ KVLite.WebApi/OutputCacheProvider.cs
@@ -52,7 +52,7 @@ namespace PommaLabs.KVLite.WebApi
         /// <summary>
         ///   The partition used by Web API output cache provider items.
         /// </summary>
-        public const string ResponseCachePartition = "KVLite.WebApi.ApiOutputCache";
+        public const string ResponseCachePartition = "KVLite.WebApi.OutputCacheProvider";
 
         #endregion Constants
 
