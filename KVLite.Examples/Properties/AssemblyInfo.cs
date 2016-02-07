c1f7bddd6b09474f1f56206ff92e7dcc6da52af5
Alessio Parma <alessio.parma@gmail.com>
2016-02-07 21:15:29 +0100

    examples

--- KVLite.Examples/Properties/AssemblyInfo.cs
+++ KVLite.Examples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Examples")]
+[assembly: AssemblyTitle("PommaLabs.KVLite.Examples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Examples")]
+[assembly: AssemblyProduct("PommaLabs.KVLite.Examples")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
