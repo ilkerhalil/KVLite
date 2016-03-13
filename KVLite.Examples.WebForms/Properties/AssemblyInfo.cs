c11cc7cc5c13c5ee87a83fba8afa9f0d6b51ecdf
Alessio Parma <alessio.parma@gmail.com>
2016-03-13 14:28:33 +0100

    updates and webforms demo

---
+++ KVLite.Examples.WebForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente set di attributi.
+// Modificare i valori di questi attributi per modificare le informazioni associate a un assembly.
+[assembly: AssemblyTitle("KVLite.Examples.WebForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KVLite.Examples.WebForms")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili ai componenti
+// COM. Se è necessario accedere a un tipo in questo assembly da COM, impostare su true l'attributo
+// ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della
+// libreria dei tipi
+[assembly: Guid("02ae0bea-762d-4034-ab7c-f7a165e282f7")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+// 
+// Versione principale Versione secondaria Numero build Revisione
+// 
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi
+// alla revisione e alla build utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
