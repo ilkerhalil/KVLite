63e4fa0f7b873f6e2211d9204cc43096e62a7667
Alessio Parma <alessio.parma@gmail.com>
2016-08-20 17:46:46 +0200

    v5.2.6.2

--- Platform Specific/KVLite.NET45/Properties/AssemblyInfo.cs
+++ Platform Specific/KVLite.NET45/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.2.0")]
-[assembly: AssemblyFileVersion("5.2.6.1")]
+[assembly: AssemblyFileVersion("5.2.6.2")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
