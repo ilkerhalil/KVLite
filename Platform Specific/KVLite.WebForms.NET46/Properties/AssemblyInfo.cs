0975833dbebf739b496e96bc9c05c5e1e9cd3f07
Alessio Parma <alessio.parma@gmail.com>
2016-09-20 08:47:37 +0200

    v5.2.8

--- Platform Specific/KVLite.WebForms.NET46/Properties/AssemblyInfo.cs
+++ Platform Specific/KVLite.WebForms.NET46/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.2.0")]
-[assembly: AssemblyFileVersion("5.2.7")]
+[assembly: AssemblyFileVersion("5.2.8")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
