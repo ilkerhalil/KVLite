ff2f54ef9a02d1f72d6af07d508c289cbc7bf8e7
Alessio Parma <alessio.parma@gmail.com>
2016-10-09 15:46:38 +0200

    fixing sqlite cache...

--- KVLite.SQLite/SQLite/SQLiteCacheConnectionFactory.cs
+++ KVLite.SQLite/SQLite/SQLiteCacheConnectionFactory.cs
@@ -21,15 +21,12 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
 // OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using CodeProject.ObjectPool;
 using LinqToDB.DataProvider;
 using LinqToDB.DataProvider.SQLite;
-using PommaLabs.Thrower;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
-using System.Diagnostics;
 
 namespace PommaLabs.KVLite.SQLite
 {
@@ -51,11 +48,6 @@ namespace PommaLabs.KVLite.SQLite
         ///   The connection string used to connect to the SQLite database.
         /// </summary>
         private string _connectionString;
-
-        /// <summary>
-        ///   The connection pool used to cache open connections.
-        /// </summary>
-        private ObjectPool<PooledConnection> _connectionPool;
 
         private readonly TSettings _settings;
 
@@ -83,106 +75,7 @@ namespace PommaLabs.KVLite.SQLite
         /// </summary>
         public IDataProvider DataProvider { get; } = new SQLiteDataProvider();
 
-        public IDbConnection Create() => _connectionPool.GetObject();
-
-        public long GetCacheSizeInKB()
-        {
-            // No need for a transaction, since it is just a select.
-            using (var db = _connectionPool.GetObject())
-            using (var cmd = db.Connection.CreateCommand())
-            {
-                cmd.CommandText = "PRAGMA page_count;";
-                var pageCount = (long) cmd.ExecuteScalar();
-
-                cmd.CommandText = "PRAGMA freelist_count;";
-                var freelistCount = (long) cmd.ExecuteScalar();
-
-                cmd.CommandText = "PRAGMA page_size;";
-                var pageSizeInKB = (long) cmd.ExecuteScalar() / 1024L;
-
-                return (pageCount - freelistCount) * pageSizeInKB;
-            }
-        }
-
-        #endregion
-
-        /// <summary>
-        ///   Runs VACUUM on the underlying SQLite database.
-        /// </summary>
-        public void Vacuum()
-        {
-            // Vacuum cannot be run within a transaction.
-            using (var db = _connectionPool.GetObject())
-            using (var cmd = db.Connection.CreateCommand())
-            {
-                cmd.CommandText = SQLiteQueries.Vacuum;
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        internal void InitConnectionString(string dataSource)
-        {
-            var builder = new SQLiteConnectionStringBuilder
-            {
-                BaseSchemaName = "kvlite",
-                FullUri = dataSource,
-                JournalMode = _journalMode,
-                FailIfMissing = false,
-                LegacyFormat = false,
-                ReadOnly = false,
-                SyncMode = SynchronizationModes.Off,
-                Version = 3,
-
-                /* KVLite uses UNIX time */
-                DateTimeFormat = SQLiteDateFormats.Ticks,
-                DateTimeKind = DateTimeKind.Utc,
-
-                /* Settings three minutes as timeout should be more than enough... */
-                DefaultTimeout = 180,
-                PrepareRetries = 3,
-
-                /* Transaction handling */
-                Enlist = false,
-                DefaultIsolationLevel = System.Data.IsolationLevel.ReadCommitted,
-
-                /* Required by parent keys */
-                ForeignKeys = true,
-                RecursiveTriggers = true,
-
-                /* Each page is 4KB large - Multiply by 1024*1024/PageSizeInBytes */
-                MaxPageCount = _settings.MaxCacheSizeInMB * 1024 * 1024 / PageSizeInBytes,
-                PageSize = PageSizeInBytes,
-                CacheSize = -2000,
-
-                /* We use a custom object pool */
-                Pooling = false,
-            };
-
-            _connectionString = builder.ToString();
-            _connectionPool = new ObjectPool<PooledConnection>(1, 10, CreatePooledConnection);
-        }
-
-        internal void EnsureSchemaIsReady()
-        {
-            using (var db = _connectionPool.GetObject())
-            using (var cmd = db.Connection.CreateCommand())
-            {
-                bool isSchemaReady;
-                cmd.CommandText = SQLiteQueries.IsSchemaReady;
-                using (var dataReader = cmd.ExecuteReader())
-                {
-                    isSchemaReady = IsSchemaReady(dataReader);
-                }
-                if (!isSchemaReady)
-                {
-                    // Creates the cache items table and the required indexes.
-                    cmd.CommandText = SQLiteQueries.CacheSchema;
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        private PooledConnection CreatePooledConnection()
+        public IDbConnection Create()
         {
 #pragma warning disable CC0022 // Should dispose object
 
@@ -202,9 +95,105 @@ namespace PommaLabs.KVLite.SQLite
 
 #pragma warning disable CC0022 // Should dispose object
 
-            return new PooledConnection(connection);
+            return connection;
 
 #pragma warning restore CC0022 // Should dispose object
+        }
+
+        public long GetCacheSizeInKB()
+        {
+            // No need for a transaction, since it is just a select.
+            using (var db = Create())
+            using (var cmd = db.CreateCommand())
+            {
+                cmd.CommandText = "PRAGMA page_count;";
+                var pageCount = (long) cmd.ExecuteScalar();
+
+                cmd.CommandText = "PRAGMA freelist_count;";
+                var freelistCount = (long) cmd.ExecuteScalar();
+
+                cmd.CommandText = "PRAGMA page_size;";
+                var pageSizeInKB = (long) cmd.ExecuteScalar() / 1024L;
+
+                return (pageCount - freelistCount) * pageSizeInKB;
+            }
+        }
+
+        #endregion IDbCacheConnectionFactory members
+
+        /// <summary>
+        ///   Runs VACUUM on the underlying SQLite database.
+        /// </summary>
+        public void Vacuum()
+        {
+            // Vacuum cannot be run within a transaction.
+            using (var db = Create())
+            using (var cmd = db.CreateCommand())
+            {
+                cmd.CommandText = SQLiteQueries.Vacuum;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        internal void InitConnectionString(string dataSource)
+        {
+            var builder = new SQLiteConnectionStringBuilder
+            {
+                BaseSchemaName = CacheSchemaName,
+                FullUri = dataSource,
+                JournalMode = _journalMode,
+                FailIfMissing = false,
+                LegacyFormat = false,
+                ReadOnly = false,
+                SyncMode = SynchronizationModes.Off,
+                Version = 3,
+
+                /* KVLite uses UNIX time */
+                DateTimeFormat = SQLiteDateFormats.Ticks,
+                DateTimeKind = DateTimeKind.Utc,
+
+                /* Settings three minutes as timeout should be more than enough... */
+                DefaultTimeout = 180,
+                PrepareRetries = 3,
+
+                /* Transaction handling */
+                Enlist = false,
+                DefaultIsolationLevel = IsolationLevel.ReadCommitted,
+
+                /* Required by parent keys */
+                ForeignKeys = true,
+                RecursiveTriggers = true,
+
+                /* Each page is 4KB large - Multiply by 1024*1024/PageSizeInBytes */
+                MaxPageCount = _settings.MaxCacheSizeInMB * 1024 * 1024 / PageSizeInBytes,
+                PageSize = PageSizeInBytes,
+                CacheSize = -2000,
+
+                /* We use a custom object pool */
+                Pooling = false,
+            };
+
+            _connectionString = builder.ToString();
+        }
+
+        internal void EnsureSchemaIsReady()
+        {
+            using (var db = Create())
+            using (var cmd = db.CreateCommand() as SQLiteCommand)
+            {
+                bool isSchemaReady;
+                cmd.CommandText = SQLiteQueries.IsSchemaReady;
+                using (var dataReader = cmd.ExecuteReader())
+                {
+                    isSchemaReady = IsSchemaReady(dataReader);
+                }
+                if (!isSchemaReady)
+                {
+                    // Creates the cache items table and the required indexes.
+                    cmd.CommandText = SQLiteQueries.CacheSchema;
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         private static bool IsSchemaReady(SQLiteDataReader dataReader)
@@ -229,53 +218,5 @@ namespace PommaLabs.KVLite.SQLite
                 && columns.Contains("kvli_parent3")
                 && columns.Contains("kvli_parent4");
         }
-
-        #region Nested type: PooledConnection
-
-        private sealed class PooledConnection : PooledObject, IDbConnection
-        {
-            public PooledConnection(SQLiteConnection connection)
-            {
-                Connection = connection;
-            }
-
-            protected override void OnReleaseResources()
-            {
-                Connection.Dispose();
-                base.OnReleaseResources();
-            }
-
-            public SQLiteConnection Connection { get; }
-
-            #region IDbConnection members
-
-            public IDbTransaction BeginTransaction() => Connection.BeginTransaction();
-
-            public IDbTransaction BeginTransaction(IsolationLevel il) => Connection.BeginTransaction(il);
-
-            public void Close() => Connection.Close();
-
-            public void ChangeDatabase(string databaseName) => Connection.ChangeDatabase(databaseName);
-
-            public IDbCommand CreateCommand() => Connection.CreateCommand();
-
-            public void Open() => Connection.Open();
-
-            public string ConnectionString
-            {
-                get { return Connection.ConnectionString; }
-                set { Connection.ConnectionString = value; }
-            }
-
-            public int ConnectionTimeout => Connection.ConnectionTimeout;
-
-            public string Database => Connection.Database;
-
-            public ConnectionState State => Connection.State;
-
-            #endregion
-        }
-
-        #endregion Nested type: PooledConnection 
     }
 }
