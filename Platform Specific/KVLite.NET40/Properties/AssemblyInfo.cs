410931bac1bc03cef4e88aa797d53f1f5f9729e7
Alessio Parma <alessio.parma@gmail.com>
2016-12-29 11:08:58 +0100

    extended copyright to 2017

--- Platform Specific/KVLite.NET40/Properties/AssemblyInfo.cs
+++ Platform Specific/KVLite.NET40/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2014-2016 Alessio Parma <alessio.parma@gmail.com>
+// Copyright (c) 2014-2017 Alessio Parma <alessio.parma@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PommaLabs")]
 [assembly: AssemblyProduct("PommaLabs.KVLite")]
-[assembly: AssemblyCopyright("Copyright (c) 2014-2016 Alessio Parma <alessio.parma@gmail.com>")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-2017 Alessio Parma <alessio.parma@gmail.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
