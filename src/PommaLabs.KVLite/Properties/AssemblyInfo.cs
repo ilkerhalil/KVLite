f472fb4621a828cb7abc4786b9306572f21d7933
Alessio Parma <alessio.parma@gmail.com>
2017-04-15 23:10:47 +0200

    ...

--- src/PommaLabs.KVLite/Properties/AssemblyInfo.cs
+++ src/PommaLabs.KVLite/Properties/AssemblyInfo.cs
@@ -31,6 +31,3 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible to COM components. If you
 // need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87427323-7ed7-4b50-9ccd-1ecb840a54db")]
