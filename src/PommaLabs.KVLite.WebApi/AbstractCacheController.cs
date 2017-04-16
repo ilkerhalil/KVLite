215c39701f933116d67038ef16f890de3457e233
Alessio Parma <alessio.parma@gmail.com>
2017-04-16 09:49:12 +0200

    src builds (y)

--- src/PommaLabs.KVLite.WebApi/AbstractCacheController.cs
+++ src/PommaLabs.KVLite.WebApi/AbstractCacheController.cs
@@ -21,9 +21,13 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
 // OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using PommaLabs.KVLite.Core;
+using PommaLabs.KVLite.Extensibility;
+using PommaLabs.Thrower;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 
 namespace PommaLabs.KVLite.WebApi
 {
