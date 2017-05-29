2b6138b5dbd2cd5661748f5211e20fc55a5318cf
Alessio Parma <alessio.parma@gmail.com>
2017-05-29 13:57:02 +0200

    v6.2.3 - ok?

--- src/PommaLabs.KVLite.SqlServer/Scripts/kvl_cache_entries.sql
+++ src/PommaLabs.KVLite.SqlServer/Scripts/kvl_cache_entries.sql
@@ -1,5 +1,4 @@
-﻿
-DROP TABLE [kvl_cache_entries];
+﻿DROP TABLE [kvl_cache_entries];
 
 
 CREATE TABLE [kvl_cache_entries] (
@@ -17,23 +16,11 @@ CREATE TABLE [kvl_cache_entries] (
     [kvle_parent_key3] NVARCHAR (255) NULL,
     [kvle_parent_key4] NVARCHAR (255) NULL,
     CONSTRAINT [pk_kvle] PRIMARY KEY CLUSTERED ([kvle_id]), 
-    CONSTRAINT [uk_kvle] UNIQUE ([kvle_partition] ASC, [kvle_key] ASC), 
-    CONSTRAINT [fk_kvle_parent0] FOREIGN KEY ([kvle_partition], [kvle_parent_key0]) REFERENCES [kvl_cache_entries]([kvle_partition], [kvle_key]) ON DELETE CASCADE,
-    CONSTRAINT [fk_kvle_parent1] FOREIGN KEY ([kvle_partition], [kvle_parent_key1]) REFERENCES [kvl_cache_entries]([kvle_partition], [kvle_key]),
-    CONSTRAINT [fk_kvle_parent2] FOREIGN KEY ([kvle_partition], [kvle_parent_key2]) REFERENCES [kvl_cache_entries]([kvle_partition], [kvle_key]),
-    CONSTRAINT [fk_kvle_parent3] FOREIGN KEY ([kvle_partition], [kvle_parent_key3]) REFERENCES [kvl_cache_entries]([kvle_partition], [kvle_key]),
-    CONSTRAINT [fk_kvle_parent4] FOREIGN KEY ([kvle_partition], [kvle_parent_key4]) REFERENCES [kvl_cache_entries]([kvle_partition], [kvle_key])
+    CONSTRAINT [uk_kvle] UNIQUE ([kvle_partition] ASC, [kvle_key] ASC)
 );
 
 
 CREATE INDEX [ix_kvle_exp_part] ON [kvl_cache_entries] ([kvle_expiry] DESC, [kvle_partition] ASC);
-
-
-CREATE INDEX [fk_kvle_parent0] ON [kvl_cache_entries] ([kvle_partition], [kvle_parent_key0]);
-CREATE INDEX [fk_kvle_parent1] ON [kvl_cache_entries] ([kvle_partition], [kvle_parent_key1]);
-CREATE INDEX [fk_kvle_parent2] ON [kvl_cache_entries] ([kvle_partition], [kvle_parent_key2]);
-CREATE INDEX [fk_kvle_parent3] ON [kvl_cache_entries] ([kvle_partition], [kvle_parent_key3]);
-CREATE INDEX [fk_kvle_parent4] ON [kvl_cache_entries] ([kvle_partition], [kvle_parent_key4]);
 
 
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Automatically generated ID.', @level0type = N'SCHEMA', @level0name = N'kvlite', @level1type = N'TABLE', @level1name = N'kvl_cache_entries', @level2type = N'COLUMN', @level2name = N'kvle_id';
