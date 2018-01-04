deae396fbe05d7de363feb2f488e397d0d447f74
Alessio Parma <alessio.parma@gmail.com>
2018-01-04 23:22:56 +0100

    setup script...

--- src/PommaLabs.KVLite.SqlServer/Scripts/appveyor_setup.ps1
+++ src/PommaLabs.KVLite.SqlServer/Scripts/appveyor_setup.ps1
@@ -2,6 +2,6 @@
 $userName = "sa"
 $password = "Password12!"
 
-$script = "$PSScriptRoot\kvl_cache_entries.sql"
+$setup = "$PSScriptRoot\kvl_cache_entries.sql"
 
 sqlcmd -S "$instance" -U "$userName" -P "$password" -I "$setup"
