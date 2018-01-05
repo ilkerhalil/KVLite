aa1641332c8f7b081398b173b226099beba476ea
Alessio Parma <alessio.parma@gmail.com>
2018-01-05 14:18:56 +0100

    appveyor setup scripts

--- src/PommaLabs.KVLite.MySql/Scripts/setup_appveyor.ps1
+++ src/PommaLabs.KVLite.MySql/Scripts/setup_appveyor.ps1
@@ -1,7 +1,7 @@
-﻿$userName      = "root"
+﻿$userName = "root"
 $env:MYSQL_PWD = "Password12!"
 
 $setup = Get-Content "$PSScriptRoot\kvl_cache_entries.sql"
 $setup = $setup -replace "`t|`n|`r", ""
 
-iex "& `"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql`" -e `"$setup`" --user=$userName"
+Invoke-Expression "& `"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql`" -e `"$setup`" --user=$userName"
--- src/PommaLabs.KVLite.PostgreSql/Scripts/setup_appveyor.ps1
+++ src/PommaLabs.KVLite.PostgreSql/Scripts/setup_appveyor.ps1
@@ -1,9 +1,9 @@
-﻿$env:PGUSER     = "postgres"
+﻿$env:PGUSER = "postgres"
 $env:PGPASSWORD = "Password12!"
-$env:PGOPTIONS  = "--client-min-messages=warning"
-$dbName         = "kvlite"
+$env:PGOPTIONS = "--client-min-messages=warning"
+$dbName = "kvlite"
 
 $setup = "$PSScriptRoot\kvl_cache_entries.sql"
 
-iex "& `"C:\Program Files\PostgreSQL\9.6\bin\createdb`" $dbName"
-iex "& `"C:\Program Files\PostgreSQL\9.6\bin\psql`" -q -d $dbName -f `"$setup`""
+Invoke-Expression "& `"C:\Program Files\PostgreSQL\9.6\bin\createdb`" $dbName"
+Invoke-Expression "& `"C:\Program Files\PostgreSQL\9.6\bin\psql`" -q -d $dbName -f `"$setup`""
