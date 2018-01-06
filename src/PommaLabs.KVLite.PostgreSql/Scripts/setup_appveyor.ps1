51107c0c942d5dbcdc5d7751fdb4d2a349e1babd
Alessio Parma <alessio.parma@gmail.com>
2018-01-06 14:43:18 +0100

    fix for postgres setup

--- src/PommaLabs.KVLite.PostgreSql/Scripts/setup_appveyor.ps1
+++ src/PommaLabs.KVLite.PostgreSql/Scripts/setup_appveyor.ps1
@@ -4,4 +4,4 @@ $env:PGOPTIONS = "--client-min-messages=warning"
 
 $setup = "$PSScriptRoot\kvl_cache_entries.sql"
 
-Invoke-Expression "& `"C:\Program Files\PostgreSQL\9.6\bin\psql`" -q -f `"$setup`""
+Invoke-Expression "& `"C:\Program Files\PostgreSQL\9.6\bin\psql`" -q -d postgres -f `"$setup`""
