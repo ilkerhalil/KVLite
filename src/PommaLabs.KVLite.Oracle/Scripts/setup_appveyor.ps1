86fd38432385390d819a8c9aa5d72a54440c39f6
Alessio Parma <alessio.parma@gmail.com>
2018-01-27 15:32:40 +0100

    oracle integ tests?

--- src/PommaLabs.KVLite.Oracle/Scripts/setup_appveyor.ps1
+++ src/PommaLabs.KVLite.Oracle/Scripts/setup_appveyor.ps1
@@ -1,3 +1,3 @@
 ﻿appveyor DownloadFile https://github.com/symfony/binary-utils/releases/download/v0.1/OracleXE112_Win64.zip
-7z x OracleXE112_Win64.zip -y >NUL
+7z x OracleXE112_Win64.zip -y > $null
 DISK1\setup.exe /s /f1"DISK1\response\OracleXE-install.iss"
